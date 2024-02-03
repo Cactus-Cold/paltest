58044147ff3278eb34c42a15127f5336af43106f
Cactus-Cold <killarrex@gmail.com>
2024-02-03 13:48:48 -0500

    batch update

--- palverctr.bat
+++ palverctr.bat
@@ -1,15 +1,25 @@
 
-call git checkout main
-call git clean -fd
-call git pull
+FOR /F "tokens=* USEBACKQ" %%F IN (`git branch --show-current`) DO (
+SET currentbranch=%%F
+)
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`git config user.name`) DO (
 SET gitName=%%F
 )
 set YYYYMMDD=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
-echo %gitName% + %YYYYMMDD%
 
-call git checkout -b %gitName%%YYYYMMDD%
-rem call git 
+if %currentbranch%=="main" (
+call git add .
+call git commit -m "Saved game"
+call git push
+)
+
+call git checkout main
+call git clean -fd
+call git pull
+
+call git branch %gitName%%YYYYMMDD%
+call git checkout %gitName%%YYYYMMDD%
+call git push --set-upstream origin %gitName%%YYYYMMDD%
 
 pause
